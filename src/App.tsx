b065d056a831ed5d0e5a433b84b5896a222776f1
Artur-FE <arturfedoseiev@gmail.com>
2025-03-25 17:53:56 +0100

    homework06

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import Lesson06 from "./lessons/Lesson06/Lesson06";
+// import Lesson06 from "./lessons/Lesson06/Lesson06";
 // import Task6 from "./lessons/Lesson06/Lesson06";
+
+import Homework06 from "./homeworks/Homework06";
 
 function App() {
 
   return (
     <>
-      <Lesson06/>
+      {/* <Lesson06/> */}
+      <Homework06 />
     </>
   )
 }
---
+++ src/homeworks/Homework06.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Input from "../components/Input/Input";
+
+function Homework06() {
+    return <Input />
+}
+
+export default Homework06;
