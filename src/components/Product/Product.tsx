fd0c8df98a09615df053e65df18586e19886347a
Artur-FE <arturfedoseiev@gmail.com>
2025-03-27 20:51:03 +0100

    homework08

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,13 +1,26 @@
 import { ProductProps } from "./types";
 import './styles.css';
 
-function Product({name, price} : ProductProps) {
+
+function Product({productName, productPrice} : ProductProps) {
     return (
-        <div className="product-cards">
-            <h2>{name}</h2>
-            <p>Price: {price}$</p>
+        <div className="product-container">
+        <div>Product: {productName}</div>
+        <div>Price: {productPrice}$</div>
         </div>
     )
 }
 
 export default Product;
+
+
+// function Product({name, price} : ProductProps) {
+//     return (
+//         <div className="product-cards">
+//             <h2>{name}</h2>
+//             <p>Price: {price}$</p>
+//         </div>
+//     )
+// }
+
+// export default Product;
---
+++ src/homeworks/Homework08/Homework08.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, useState } from "react";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
+import './styles.css';
+
+function Homework08() {
+  const [userPassword, setUserPassword] = useState<string>('');
+  const [showUserPassword, getShowUserPassword] = useState<boolean>(false);
+
+  const onChangePassword = (event:ChangeEvent<HTMLInputElement>) => {
+    setUserPassword(event.target.value);
+  }
+
+  const showPassword = () => {
+    getShowUserPassword(!showUserPassword);
+  }
+
+  let nameButton = 'SHOW PASSWORD';
+  showUserPassword? nameButton = 'HIDE PASSWORD' : 'SHOW PASSWORD';
+
+  return (
+    <div className="hw08-container">
+      <h1>Change password</h1>
+      <Input
+        name="password"
+        label="password"
+        type="password"
+        placeholder="Enter your password"
+        value={userPassword}
+        onChange={onChangePassword}
+      />
+
+      {showUserPassword && <div className="password-h2"><h2>Your password</h2> 
+      <Input name="userPassword" value={userPassword} /></div>}
+
+      <Button name={nameButton} onClick={showPassword}/>
+      
+    </div>
+  );
+}
+
+export default Homework08;
