af6f65cd92f1adaae629b366ee30cb31787c3824
Artur-FE <arturfedoseiev@gmail.com>
2025-03-27 09:29:07 +0100

    add key

--- src/homeworks/Homework07/Homework07.tsx
+++ src/homeworks/Homework07/Homework07.tsx
@@ -1,10 +1,11 @@
+import { v4 } from "uuid";
 import Product from "../../components/Product/Product";
 import { products } from "./dataHomeWork07";
 import './style.css'
 
   function Homeworks07() {
     const productCards = products.map((product)=> {
-        return <Product name={product.name} price={product.price} />
+        return <Product key={v4()} name={product.name} price={product.price} />
     })
 
     return (
