53738b5bdb6fe6b0ee8129b4742fb782e43e8bda
Artur-FE <arturfedoseiev@gmail.com>
2025-04-01 20:31:53 +0200

    homework11

---
+++ src/homeworks/Homework11/Homework11.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import Input from "../../components/Input/Input";
+import { Homework11Container, ResultBox, ResultValue } from "./styles";
+
+function Homework11() {
+  const [controlInputValue, setControlInputValue] = useState<string>("");
+  const [resultInputValue, setResultInputValue] = useState<number>(0);
+  const [firstRender, setFirstRender] = useState<boolean>(true);
+
+  const controlInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setControlInputValue(event.target.value);
+  };
+
+  useEffect(() => {
+    setFirstRender(false);
+  }, []);
+
+  useEffect(() => {
+    if (!firstRender) {
+      setResultInputValue(resultInputValue + 1);
+    }
+  }, [controlInputValue]);
+
+  return (
+    <Homework11Container>
+      <Input name="controlInput" onChange={controlInputChange} />
+      <Input name="resultInput" />
+      <ResultBox>
+        Внесено изменений в первый Input:
+        <ResultValue>{resultInputValue}</ResultValue>
+      </ResultBox>
+    </Homework11Container>
+  );
+}
+
+export default Homework11;
